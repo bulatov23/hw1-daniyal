40da12f5ff657c67c5907dbded8380b5101e75ab
bulatov23 <bulatov.daniyal1@gmail.com>
2023-09-29 09:56:31 +0300

    HW2 is completed update 15

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,7 +10,7 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func validate(str string) bool {
+func validate(str string) bool { // здесь я написал функцию которая валидирует строку на некорректные значени, такие как цифра в первом символе или число больше 9)
 	strArr := [10]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	for _, s := range strArr {
 		if string(str[0]) == s {
@@ -28,25 +28,31 @@ func validate(str string) bool {
 	return true
 }
 
-func Unpack(str string) (string, error) {
+func Unpack(str string) (string, error) { // здесь получаю строку, вызываю функцию валидации и распаковываю строку
+	runes := []rune(str)
 	if str == "" {
 		return "", nil
 	}
 	val := validate(str)
-	if !val {
-		return "", ErrInvalidString
+	if val == false {
+		return "некорректная строка", nil
 	}
+	//var builder strings.Builder
 	previousChar := ""
-	for ind, char := range str {
-		count, _ := strconv.Atoi(string(char))
-		if count == 0 && unicode.IsDigit(char) {
-			previousChar = str[:ind-1]
-			continue
-		}
+	for ind, char := range runes {
 		if unicode.IsDigit(char) {
-			previousChar += strings.Repeat(string(str[ind-1]), count-1)
+			count, _ := strconv.Atoi(string(char))
+			if count == 0 {
+				previousChar = string(runes[:ind-1])
+			} else if count > 0 {
+				if string(str[ind-1]) == "\n" {
+					previousChar += strings.Repeat("\\n", count-1)
+				} else {
+					previousChar += strings.Repeat(string(runes[ind-1]), count-1)
+				}
+			}
 		} else if !unicode.IsDigit(char) {
-			if char == '\n' {
+			if string(char) == "\n" {
 				previousChar += "\\n"
 			} else {
 				previousChar += string(char)
