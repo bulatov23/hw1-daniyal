ede38b830e3c3e9b8431d2a30b9058c626b78edb
daniyal.b.bulatov <daniyal.b.bulatov@sbermarket.ru>
2023-11-10 09:02:26 +0300

    hw3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,30 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	m := make(map[string]int)
+	x := strings.Split(str, " ")
+	var sort_str []string
+	for _, v := range x {
+		m[v] += 1
+	}
+	type key_value struct {
+		Key   string
+		Value int
+	}
+	var sorted_struct []key_value
+	for key, value := range m {
+		sorted_struct = append(sorted_struct, key_value{key, value})
+	}
+	sort.Slice(sorted_struct, func(i, j int) bool {
+		return sorted_struct[i].Value > sorted_struct[j].Value
+	})
+	for _, key_value := range sorted_struct {
+		sort_str = append(sort_str, key_value.Key)
+	}
+	return sort_str
 }
