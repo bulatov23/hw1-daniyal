a960264760c43871b0bc43f46d7d6aa3fc2c03d5
daniyal.b.bulatov <daniyal.b.bulatov@sbermarket.ru>
2023-11-13 08:57:21 +0300

    hw3 added sorted 5

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -8,29 +8,29 @@ import (
 func Top10(str string) []string {
 	m := make(map[string]int)
 	x := strings.Fields(str)
-	sort_str := make([]string, 0, 20)
+	sortStr := make([]string, 0, 20)
 	for _, v := range x {
-		m[v] += 1
+		m[v]++
 	}
-	type key_value struct {
+	type keyValue struct {
 		Key   string
 		Value int
 	}
-	sorted_struct := make([]key_value, 0, 20)
+	sortedStruct := make([]keyValue, 0, 20)
 	for key, value := range m {
-		sorted_struct = append(sorted_struct, key_value{key, value})
+		sortedStruct = append(sortedStruct, keyValue{key, value})
 	}
-	sort.Slice(sorted_struct, func(i, j int) bool {
-		if sorted_struct[i].Value == sorted_struct[j].Value {
-			return sorted_struct[i].Key < sorted_struct[j].Key
+	sort.Slice(sortedStruct, func(i, j int) bool {
+		if sortedStruct[i].Value == sortedStruct[j].Value {
+			return sortedStruct[i].Key < sortedStruct[j].Key
 		}
-		return sorted_struct[i].Value > sorted_struct[j].Value
+		return sortedStruct[i].Value > sortedStruct[j].Value
 	})
-	for i, key_value := range sorted_struct {
+	for i, keyValue := range sortedStruct {
 		if i > 9 {
 			break
 		}
-		sort_str = append(sort_str, key_value.Key)
+		sortStr = append(sortStr, keyValue.Key)
 	}
-	return sort_str
+	return sortStr
 }
